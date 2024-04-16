a9353e9678389cd4be8cc49a2262585f93ac758b
Shrayanssh jain <44131623+Shrayansshjain@users.noreply.github.com>
2024-04-16 18:02:16 +0530

    Add files via upload

---
+++ JAVA/Swap.java
@@ -0,0 +1,20 @@
+package com;
+
+import java.util.Scanner;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		swap(a,b);
+	}
+	static void swap(int a, int b) {
+		a = a+b;
+		b = a-b;
+		a = a-b;
+		System.out.println(a+","+b);
+	}
+}
