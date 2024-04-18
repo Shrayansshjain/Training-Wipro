c54717f2995a4475701a8c684a1033dd2617ce26
Shrayanssh jain <44131623+Shrayansshjain@users.noreply.github.com>
2024-04-18 10:39:40 +0530

    Add files via upload

---
+++ Daily task assignment/Exception2.java
@@ -0,0 +1,17 @@
+package exception;
+
+public class Exception2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {1,2,3,4,5};
+		try {
+			int a = arr[8];
+			
+		}
+		catch(ArrayIndexOutOfBoundsException ae) {
+			
+		}
+	}
+
+}
