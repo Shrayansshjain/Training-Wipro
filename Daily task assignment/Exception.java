099c84a104c855e271d053ba310e49dfd31a36e9
Shrayanssh jain <44131623+Shrayansshjain@users.noreply.github.com>
2024-04-18 10:40:48 +0530

    Add files via upload

---
+++ Daily task assignment/Exception.java
@@ -0,0 +1,28 @@
+package exception;
+//import java.lang.ArrayIndexOutOfBoundsException;
+public class Exception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			int a[] = {1,2,3,4,5};
+			System.out.println(a[0]);
+			int result = 10/0;
+			String s = null;
+			s.length();
+		}
+		catch(ArrayIndexOutOfBoundsException ae) {
+			System.out.println("Index bahar ho gaya mr "+ ae.getMessage());
+		}
+		catch(NullPointerException e) {
+			System.out.println("Null aara hai mr "+ e.getMessage());
+		}
+		catch(ArithmeticException e) {
+			System.out.println("Maths gadbad ho gaya mr "+ e.getMessage());
+		}
+		finally {
+			System.out.println("Ok tata bye bye phirse aana ");
+		}
+	}
+
+}
